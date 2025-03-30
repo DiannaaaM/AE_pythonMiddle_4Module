48156eafa399b211b1f241948543cf792ff894f4
DiannaaaMmrrr <murlaevadiana@gmail.com>
2025-03-30 18:57:16 +0300

    fix some mistakes

--- lesson_31/main.py
+++ lesson_31/main.py
@@ -1,47 +1,66 @@
 import pygame
 
 
+# Определяет класс питомеца
 class Pet:
+    """
+    Класс, описывающий питомеца.
+    """
     def __init__(self, x, y, health, max_health, happiness, max_happiness):
-        # Set up basic fields
+        """
+        Инициализирует объект питомеца.
+        """
         self.x = x
         self.y = y
-        # Health = radius
+        # Здоровье питомеца - это радиус окружности
         self.health = health
         self.max_health = max_health
-        # Happiness = green value of colour
+        # Счастье питомеца - это зеленый цвет цвета
         self.happiness = happiness
         self.max_happiness = max_happiness
         self.colour = pygame.Color(0, happiness, 0)
 
-    # Return the x and y positions (center of circle) as a 2D vector
+    # Возвращает координаты центра окружности и прямоугольник, ограничивающий окружность
     def get_pos(self):
+        """
+        Возвращает координаты центра окружности и прямоугольник, ограничивающий окружность.
+        """
         return pygame.Vector2(self.x, self.y)
 
-    # Return a rectangle surrounding the circle where the x and y positions are center - radius and the width and height are radius * 2
     def get_rect(self):
         return pygame.Rect(self.x - self.health, self.y - self.health, self.health * 2, self.health * 2)
-
-    # Increase/decrease the x and/or y positions
+    # Изменяет координаты питомеца
     def move(self, x_amount, y_amount):
+        """
+        Изменяет координаты питомеца.
+        """
         self.x += x_amount
         self.y += y_amount
 
-    # Update health and happiness by consuming item
+    # Обновляет здоровье и счастье питомеца, потребляя предмет
     def consume_item(self, item):
+        """
+        Обновляет здоровье и счастье питомеца, потребляя предмет.
+        """
         self.update_health(item.health)
         self.update_happiness(item.happiness)
 
-    # Update health (radius) and make sure it doesn't exceed max or min
+    # Обновляет здоровье и счастье питомеца
     def update_health(self, d_h):
+        """
+        Обновляет здоровье и счастье питомеца.
+        """
         self.health += d_h
         if self.health > self.max_health:
             self.health = self.max_health
         elif self.health < 0:
             self.health = 0
 
-    # Update happiness (green colour) and make sure it doesn't exceed max or min
+    # Обновляет счастье питомеца
     def update_happiness(self, d_h):
+        """
+        Обновляет счастье питомеца.
+        """
         self.happiness += d_h
         if self.happiness > self.max_happiness:
             self.happiness = self.max_happiness
@@ -49,42 +68,62 @@ class Pet:
             self.happiness = 0
         self.colour = pygame.Color(0, self.happiness, 0)
 
+    # Проверяет, жив ли питомец
     def check_if_dead(self):
+        """
+        Проверяет, жив ли питомец.
+        """
         return self.health <= 0 or self.happiness <= 0
 
 
+# Определяет класс предмета
 class Item:
+    """
+    Класс, описывающий предмет.
+    """
     def __init__(self, x, y, health, happiness, image_name):
-        # Set up basic fields
+        """
+        Инициализирует объект предмета.
+        """
         self.x = x
         self.y = y
         self.health = health
         self.happiness = happiness
-        # Load and store the image based on the filepath
+        # Загружает и сохраняет изображение по имени файла
         self.image = pygame.image.load(image_name)
-        # Shift the image rect so that the x and y are at the center rather than top left
+        # Сдвигает изображение так, чтобы координаты x и y были в центре, а не в верхнем левом углу
         rect = self.image.get_rect()
         self.image_rect = pygame.Rect(x - rect.width / 2, y - rect.height / 2, rect.width, rect.height)
 
 
+# Определяет класс игры
 class Game:
+    """
+    Класс, описывающий игру.
+    """
     def __init__(self):
-        # Display variables
+        """
+        Инициализирует объект игры.
+        """
+        # Размеры окна
         self.width = 500
         self.height = 500
+        # Цвет фона
         self.background_colour = "white"
+        # Высота полосы кнопок
         self.buttons_bar_height = 100
+        # Цвет полосы кнопок
         self.buttons_bar_colour = "orange"
-        # Pygame specific variables
+        # Переменные, связанные с Pygame
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Pygame Pet")
         self.clock_tick = 60
         self.clock = pygame.time.Clock()
-        # Item variables
+        # Переменные, связанные с предметами
         self.image_names = ["apple.png", "icecream.png", "toy.png"]
         self.item_mode_index = 0
         self.item = None
-        # Button variables
+        # Переменные, связанные с питомецем
         self.apple_button = Item(self.width / 4, self.buttons_bar_height / 2, 0, 0, self.image_names[0])
         self.ice_cream_button = Item(self.width / 2, self.buttons_bar_height / 2, 0, 0, self.image_names[1])
         self.toy_button = Item(self.width * (3 / 4), self.buttons_bar_height / 2, 0, 0, self.image_names[2])
@@ -98,127 +137,147 @@ class Game:
         self.size_update_rate = self.clock_tick / 3
         self.colour_update_rate = self.clock_tick / 10
 
-    # Select an item or place an item if appropriate area is clicked
+    # Выбирает предмет или создает предмет, если подходящая область нажата
     def handle_mouse_click(self):
+        """
+        Выбирает предмет или создает предмет, если подходящая область нажата.
+        """
         pos = pygame.mouse.get_pos()
-        # Check for button press
+        # Проверяет, был ли нажат кнопка в области кнопок
         if self.apple_button.image_rect.collidepoint(pos):
             self.item_mode_index = 0
         elif self.ice_cream_button.image_rect.collidepoint(pos):
             self.item_mode_index = 1
         elif self.toy_button.image_rect.collidepoint(pos):
             self.item_mode_index = 2
-        # Do nothing if user clicks button bar outside of buttons
+        # Не делает ничего, если пользователь нажал кнопку вне области кнопок
         elif pos[1] < self.buttons_bar_height:
             return
-        # Create an item at the mouse position
+        # Создает предмет в выбранной позиции
         else:
             self.create_item(pos)
 
-    # Spawn an item at the position
+    # Создает предмет в выбранной позиции
     def create_item(self, pos):
-        # Get current image name
+        """
+        Создает предмет в выбранной позиции.
+        """
+        # Загружает имя нового предмета
         image_name = self.image_names[self.item_mode_index]
-        # Create an item at the position
+        # Создает предмет в выбранной позиции
         if self.item_mode_index == 0:
             self.item = Item(pos[0], pos[1], 20, 0, image_name)
         elif self.item_mode_index == 1:
             self.item = Item(pos[0], pos[1], -10, 60, image_name)
         elif self.item_mode_index == 2:
             self.item = Item(pos[0], pos[1], 0, 40, image_name)
-        # Start moving the pet
+        # Начинает двигать питомец
         self.set_speed()
 
-    # Set the magnitude and direction of pet movement
+    # Устанавливает скорость и направление движения питомеца
     def set_speed(self):
-        # Get differences in x and y positions of pet and item
+        """
+        Устанавливает скорость и направление движения питомеца.
+        """
+        # Вычисляет разницу в x и y координатах питомеца и предмета
         d_x = abs(self.pet.x - self.item.x)
         d_y = abs(self.pet.y - self.item.y)
-        # Check whether x difference is greater than y difference
+        # Проверяет, больше ли разница в x координатах, чем в y координатах
         if d_x >= d_y:
             self.d_x = self.speed
-            # Slow down the y movement
+            # Снижает скорость y движения в соответствии с разницей в x координатах
             self.d_y = self.speed * (d_y / d_x)
         else:
-            # Slow down the x movement
+            # Снижает скорость x движения в соответствии с разницей в y координатах
             self.d_x = self.speed * (d_x / d_y)
             self.d_y = self.speed
-        # Set x speed to negative if item is further left than pet
+        # Если питомец находится левее предмета, устанавливает скорость x в отрицательную сторону
         if self.pet.x > self.item.x:
             self.d_x = -self.d_x
-        # Set y speed to negative if item is further up than pet
+        # Если питомец находится выше предмета, устанавливает скорость y в отрицательную сторону
         if self.pet.y > self.item.y:
             self.d_y = -self.d_y
 
-    # Check for collisions between pet and item
+    # Проверяет столкновение питомеца и предмета
     def handle_item_collision(self):
-        # If item exists and item.rect and pet.rect overlap, collision occurred
+        """
+        Проверяет столкновение питомеца и предмета.
+        """
+        # Если предмет существует и область предмета пересекается с областью питомеца, произошло столкновение
         if self.item != None and self.item.image_rect.colliderect(self.pet.get_rect()):
-            # Consume then delete item
+            # Убирает предмет и делает питомеца неподвижным
             self.pet.consume_item(self.item)
             self.item = None
-            # Stop pet movement by setting speeds to 0
             self.d_x = 0
             self.d_y = 0
 
-    # Update pet position, health, and happiness
+    # Обновляет координаты, здоровье и счастье питомеца
     def update_pet(self):
-        # Position
+        """
+        Обновляет координаты, здоровье и счастье питомеца.
+        """
+        # Обновляет координаты питомеца
         self.pet.move(self.d_x, self.d_y)
-        # Update health and happiness
+        # Обновляет здоровье и счастье питомеца
         self.current_tick += 1
-        # Decay the health 3x per second
+        # Уменьшает здоровье питомеца на 3 раза в секунду
         if self.current_tick % self.size_update_rate == 0:
             self.pet.update_health(self.decay_rate)
-        # Decay the happiness 10x per second
+        # Уменьшает счастье питомеца на 10 раз в секунду
         if self.current_tick % self.colour_update_rate == 0:
             self.pet.update_happiness(self.decay_rate)
-        # Reset current tick to prevent it becoming too large
+        # Сбрасывает текущий таймер, чтобы он не стал слишком большим
         if self.current_tick == 60:
             self.current_tick = 0
 
-    # Draw the screen, bar, buttons, item, pet
+    # Рисует окно, полосу кнопок, кнопки, предмет, питомец
     def draw_everything(self):
-        # Screen
+        """
+        Рисует окно, полосу кнопок, кнопки, предмет, питомец.
+        """
+        # Очищает окно
         self.screen.fill(self.background_colour)
-        # Item
+        # Рисует предмет, если он существует
         if self.item != None:
             self.screen.blit(self.item.image, self.item.image_rect)
-        # Pet
+        # Рисует питомец
         pygame.draw.circle(self.screen, self.pet.colour, self.pet.get_pos(), self.pet.health)
-        # Buttons bar
+        # Рисует полосу кнопок
         pygame.draw.rect(self.screen, self.buttons_bar_colour, pygame.Rect(0, 0, self.width, self.buttons_bar_height))
-        # Buttons
+        # Рисует кнопки
         self.screen.blit(self.apple_button.image, self.apple_button.image_rect)
         self.screen.blit(self.ice_cream_button.image, self.ice_cream_button.image_rect)
         self.screen.blit(self.toy_button.image, self.toy_button.image_rect)
-        # Update
+        # Обновляет окно
         pygame.display.update()
 
-    # Run the game loop
+    # Запускает игру
     def run(self):
+        """
+        Запускает игру.
+        """
         while True:
-            # Handle incoming events
+            # Обрабатывает входящие события
             for event in pygame.event.get():
-                # Quit event
+                # Если произошло закрытие окна, завершает работу программы
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     return
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     self.handle_mouse_click()
-            # Detect pet-item collision
+            # Проверяет столкновение питомеца и предмета
             self.handle_item_collision()
-            # Check if pet is dead and game over
+            # Проверяет, жив ли питомец и завершает работу программы, если он мерт
             if self.pet.check_if_dead():
                 pygame.quit()
                 return
-            # Update
+            # Обновляет состояние питомеца
             self.update_pet()
 
-            # Draw
+            # Рисует все элементы на экране
             self.draw_everything()
 
-            # Tick clock
+            # Обновляет кадр
             self.clock.tick(self.clock_tick)
 
 
